eb4bcfdc4bdd053e9e110cd720777dd1e4c0c993
HANZAL SALIM <hanzalsalim94@gmail.com>
2015-07-14 16:18:32 +0530

    working on image download

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, request, render_template, url_for, redirect, flash, session, g, send_file, abort
+import requests
+
+import shutil
+
+import requests
 
 
 app = Flask(__name__)
@@ -8,6 +13,25 @@ app = Flask(__name__)
 def index():
 	if request.method == 'GET':
 		return render_template('index.html')
+	if request.method == 'POST':
+		app.logger.info("Fuck this shit")
+		image1 = request.form['image1']
+		image2 = request.form['image2']
+		print image1, image2
+		response = requests.get(image1, stream=True)
+		with open('img1.png', 'wb') as out_file:
+		    shutil.copyfileobj(response.raw, out_file)
+		del response
+		response = requests.get(image2, stream=True)
+		with open('img2.png', 'wb') as out_file:
+		    shutil.copyfileobj(response.raw, out_file)
+		del response
+
+		return render_template('index.html')
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5050)
