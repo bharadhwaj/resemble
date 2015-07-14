30a589a19abb172ae820d9cd48d50c40828913a9
Hanzal Salim <hanzalsalim94@gmail.com>
2015-07-14 17:14:55 +0530

    Working state

--- app.py
+++ app.py
@@ -5,30 +5,32 @@ import shutil
 
 import requests
 
-
 app = Flask(__name__)
-
+app.config.from_object('config')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'GET':
 		return render_template('index.html')
 	if request.method == 'POST':
-		app.logger.info("Fuck this shit")
+
 		image1 = request.form['image1']
 		image2 = request.form['image2']
 		print image1, image2
 		response = requests.get(image1, stream=True)
-		with open('img1.png', 'wb') as out_file:
+		with open('static/imgs/img1', 'wb') as out_file:
 		    shutil.copyfileobj(response.raw, out_file)
 		del response
 		response = requests.get(image2, stream=True)
-		with open('img2.png', 'wb') as out_file:
+		with open('static/imgs/img2', 'wb') as out_file:
 		    shutil.copyfileobj(response.raw, out_file)
 		del response
 
-		return render_template('index.html')
+		return redirect(url_for('result'))
 
+@app.route('/result')
+def result():
+	return render_template('result.html')
 
 
 
